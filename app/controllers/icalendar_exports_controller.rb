0edb0663362fdda6d4e03c5b0fa1baf7f4f713d1
Tom Dooner <tomdooner@gmail.com>
2021-01-21 22:33:52 -0800

    Re-add DTEND and add X-WR-TIMEZONE

--- app/controllers/icalendar_exports_controller.rb
+++ app/controllers/icalendar_exports_controller.rb
@@ -6,11 +6,13 @@ class ICalendarExportsController < ApplicationController
   def show
     cal = Icalendar::Calendar.new
     cal.x_wr_calname = 'Ski Reservations (Automatic)'
+    cal.x_wr_timezone = 'America/Los_Angeles'
 
     @configs.flat_map do |config|
       config.user.reservations.map do |reservation|
         cal.event do |e|
           e.dtstart = Icalendar::Values::Date.new(reservation.reservation_date)
+          e.dtend = Icalendar::Values::Date.new(reservation.reservation_date + 1)
           e.summary = "#{reservation.resort_name} (#{config.display_name})"
           e.description = "Last fetched: #{reservation.fetched_at}"
         end
