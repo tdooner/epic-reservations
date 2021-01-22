c0e8a7dc057cab1b1c0dd3b31297ddbe14b32bbc
Tom Dooner <tomdooner@gmail.com>
2021-01-21 22:00:50 -0800

    Actually implement ICalendar config saving

--- app/controllers/icalendar_configs_controller.rb
+++ app/controllers/icalendar_configs_controller.rb
@@ -1,13 +1,23 @@
 class ICalendarConfigsController < ApplicationController
+  before_action :set_config
+
   def show
-    @config = current_user.icalendar_config || current_user.build_icalendar_config
   end
   
   def update
+    if @config.update(icalendar_config_params)
+      flash[:info] = 'Success!'
+    end
+    redirect_to icalendar_configs_path
   end
 
   private
 
+  def set_config
+    @config = current_user.icalendar_config || current_user.build_icalendar_config
+  end
+
   def icalendar_config_params
+    params.fetch(:icalendar_config, {}).permit(:public_share_code, :display_name)
   end
 end
