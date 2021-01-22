b9d7c171dc7eeba02726db064d80013acbb62144
Tom Dooner <tomdooner@gmail.com>
2021-01-21 22:55:47 -0800

    Add reservation list

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resource :epic_logins, only: %i[show update]
   resource :icalendar_configs, only: %i[show update]
+  resource :reservations, only: %i[show]
 
   get '/ical_export/:public_share_code' => 'icalendar_exports#show', as: :ical_export
 
