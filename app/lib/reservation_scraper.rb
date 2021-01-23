45a15c2f737f33d07c61dd142b2b50a121a59688
Tom Dooner <tomdooner@gmail.com>
2021-01-22 19:36:41 -0800

    Refactor wait_for out of scraper

--- app/lib/reservation_scraper.rb
+++ app/lib/reservation_scraper.rb
@@ -12,6 +12,7 @@ class ReservationScraper
     @username = epic_username
     @password = epic_password
     @retries = 3
+    @wait = Selenium::WebDriver::Wait.new(timeout: 20)
   end
 
   def reservations
@@ -21,29 +22,26 @@ class ReservationScraper
     chrome_opts = Selenium::WebDriver::Chrome::Options.new
     chrome_opts.add_argument('--window-size=1440,1000')
     chrome_opts.add_argument('--disable-dev-shm-usage')
-    wait = Selenium::WebDriver::Wait.new(timeout: 10)
 
     driver = Selenium::WebDriver.for :chrome, options: chrome_opts
     puts "Beginning scrape for #{@username}"
     driver.navigate.to 'https://www.epicpass.com/account/my-account.aspx'
-
-    wait.until { driver.find_element(id: 'onetrust-accept-btn-handler') }
+    wait_for(id: 'onetrust-accept-btn-handler')
 
     puts 'Filling in form'
     driver.find_element(id: 'onetrust-accept-btn-handler').click # cookie banner
     driver.find_element(id: 'txtUserName_3').send_keys(@username)
     driver.find_element(id: 'txtPassword_3').send_keys(@password)
     driver.find_element(css: '#returningCustomerForm_3 .primaryCTA').click
+    wait_for(css: 'h1.title')
 
-    wait.until { driver.find_element(css: 'h1.title') } # wait for page to load
     puts 'Logged In'
-
     driver.navigate.to 'https://www.epicpass.com/account/my-account.aspx?ma_1=4'
-    wait.until { driver.find_element(class: 'season_passes__reservations__title--firstPass') }
+    wait_for(class: 'season_passes__reservations__title--firstPass')
     puts 'Loaded "Reservations" tab'
     driver.find_element(class: 'season_passes__reservations__title--firstPass').click
 
-    wait.until { driver.find_element(css: '.season_passes__reservations__content__list > li') }
+    wait_for(css: '.season_passes__reservations__content__list > li')
     reservation_list = driver.find_elements(css: '.season_passes__reservations__content__list > li')
     puts 'Loaded inner dropdown'
 
@@ -59,8 +57,15 @@ class ReservationScraper
     @retries -= 1
     puts "Got exception #{ex}"
     if @retries > 0
-      puts 'Retrying...'
+      puts 'Retrying in 20 seconds...'
       retry if @retries > 0
     end
+  end
+
+  def wait_for(selector)
+    @wait.until { driver.find_element(selector) }
+  rescue Selenium::WebDriver::Error::TimeoutError => ex
+    puts "Timeout looking for #{selector}: #{ex}"
+    puts "Page text: #{driver.find_element(css: 'body').text}"
   end
 end
