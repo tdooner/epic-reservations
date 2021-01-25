745eb3bb8155d8d343b86a3c71daaee1d4af0ea5
Tom Dooner <tomdooner@gmail.com>
2021-01-24 20:22:06 -0800

    Add weather forecasts to reservations page

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,6 @@
 class ReservationsController < ApplicationController
   def show
     @reservations = current_user.reservations
+    @forecasts = WeatherForecast.from_reservations(@reservations).all
   end
 end
--- app/models/weather_forecast.rb
+++ app/models/weather_forecast.rb
@@ -1,2 +1,10 @@
 class WeatherForecast < ApplicationRecord
+  def self.from_reservations(reservations)
+    reservations.reduce(WeatherForecast.none) do |scope, i|
+      scope.or(WeatherForecast.where(
+        date: i.reservation_date,
+        resort_name: i.resort_name
+      ))
+    end
+  end
 end
